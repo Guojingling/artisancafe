f66474c3f5a7d183e253d465f7e466a1009beff4
Evan Shi <77384763+ZoshiCoding@users.noreply.github.com>
2025-04-15 16:53:03 +1000

    migrated project to new react type

---
+++ ClientApp/app/pages/about/info.tsx
@@ -0,0 +1,11 @@
+export function Info() {
+    return (
+        <div>
+            <h1>Information Page</h1>
+            <h2>Address: </h2>
+            <h2>Opening hours:</h2>
+            <h2>Email: </h2>
+            <h2>Phone: </h2>
+        </div>
+    )
+}
